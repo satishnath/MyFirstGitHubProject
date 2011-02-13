c96c80c720b5af776ce059604d3d613bfdfc1fb5
satishnath <satishnath@gmail.com>
2011-02-13 17:13:51 -0600

    Created another model Address

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MyFirstGitHubProject::Application.routes.draw do
-  resources :users
+  resources :addresses
+
+ # resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
